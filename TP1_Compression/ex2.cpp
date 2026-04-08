d8ddea7955a860858eebc7248820df25369073fa
TheoGar <theo.garnaud@telecom-sudparis.eu>
2026-04-08 12:13:20 +0200

    Add DPCM encoding and decoding functions

--- TP1_Compression/ex2.cpp
+++ TP1_Compression/ex2.cpp
@@ -7,8 +7,12 @@
 #include "pred.h" 
 #include "dct.h"
 
- void my_codeurDPCM(unsigned char **x, int **err, int H, int W, int step); 
- void my_decodeurDPCM(int **err, unsigned char **xrec, int H, int W); 
+void my_codeurDPCM(unsigned char **x, int **err, int H, int W, int step); 
+void my_decodeurDPCM(int **err, unsigned char **xrec, int H, int W); 
+ 
+void my_codeurDPCM_retro(unsigned char **x, int **err, int H, int W, int step); 
+void my_decodeurDPCM_reto(int **err, unsigned char **xrec, int H, int W); 
+
 
 int SaveIntImage_pgm(char *nom, int **im, int Height, int Width)
 { 
@@ -34,6 +38,283 @@ int SaveIntImage_pgm(char *nom, int **im, int Height, int Width)
   
   ecriture_pgm(nom, ima_uc, Width, Height); 
   
+  dalocuc(ima_uc, Height); 
+  
+  return 1; 
+}
+
+
+int SaveIntImage_pgm_tronc(char *nom, int **im, int Height, int Width)
+{
+  int i, j;
+  
+    unsigned char ** ima_uc = alocamuc(Height, Width); 
+
+  
+  for(i = 0; i<Height; i++)
+    for(j = 0; j<Width; j++)
+      {
+	if( abs(im[i][j]) >= 255.0 )
+	  ima_uc[i][j] = 255; 
+	else
+	  ima_uc[i][j] = (unsigned char) ( (abs(im[i][j])) );	
+      }
+  
+  ecriture_pgm(nom, ima_uc, Width, Height); 
+  
+  dalocuc(ima_uc, Height); 
+  
+  return 1; 
+}
+
+
+
+
+int main (int argc, char *argv[])
+{ 
+  char nom[200], nom_out[200], nom_err[300]; 
+  int W, H; // les dimensions de li'image: H = Height, W = Width
+  int i, j; 
+
+  if (argc != 3)
+    { 
+      fprintf(stderr, "Utilisation: %s <nom_image_pgm> <pas_quantification> \n", argv[0]); exit(0); 
+    }
+
+  strcpy(nom, argv[1]); 
+  strcpy(nom_out, argv[1]); strcat(nom_out, ".out"); 
+  strcpy(nom_err, argv[1]); strcat(nom_err, ".err"); 
+
+  int step = atoi(argv[2]); 
+  
+  lecture_dim(nom, &W, &H);
+
+  fprintf(stderr, "Width = %d  Height = %d\n", W, H);
+
+  unsigned char **x = alocamuc(H, W);
+  unsigned char **xrec = alocamuc(H, W);
+
+  lecture_pgm(nom, x); // x contient l'image initiale
+
+  int **err = alocami(H, W); // allocation de la matric des erreurs de prediction
+
+  for(i=0;i<H;i++)
+    for(j=0;j<W;j++)
+	err[i][j] = (int)x[i][j]; 
+   fprintf(stderr, "\nentropie initiale = %g [bits/pixel]\n", calc_entropie(err, H, W));
+
+
+  
+   /*
+  // CODE DCT
+  double **tdct = alocamd(H, W); // image transformee
+  double **xd = alocamd(H, W);   // image initiale en double
+  double **xrecd = alocamd(H, W); // image reconstruite
+
+  
+
+  for(i=0;i<H;i++)
+    for(j=0;j<W;j++)
+      xd[i][j] = (double)x[i][j]; 
+
+
+  dct2dim(xd, tdct, H, W);
+  fprintf(stderr, "OK DCT directe\n"); 
+  
+//Quantification des coefficients de la DCT
+  for(i=1;i<H;i++)
+    for(j=1;j<W;j++)
+      {	
+	err[i][j] = quantiz(tdct[i][j], step);
+	tdct[i][j] = (double)err[i][j];	
+      }
+   for(j=0;j<W;j++)
+     {err[0][j] =  quantiz(tdct[0][j], 1); tdct[0][j] = (double)err[0][j];}
+   for(i=0;i<H;i++)
+     {err[i][0] =  quantiz(tdct[i][0], 1); tdct[i][0] = (double)err[i][0];}
+     
+    //FIN Quantification des coefficients de la DCT
+ 
+
+    double entro = calc_entropie(err, H, W); 
+    fprintf(stderr, "\nentropie = %g [bits/pixel]\n", entro);
+  
+  dct2dim_inv(tdct, xrecd, H, W);
+  for(i=0;i<H;i++)
+    for(j=0;j<W;j++)
+      if(xrecd[i][j] < 0.0)  xrec[i][j] = 0;
+      else if(xrecd[i][j] > 255.0)  xrec[i][j] = 255;
+      else  xrec[i][j] = (unsigned char)xrecd[i][j]; 
+  SaveIntImage_pgm_tronc(nom_err, err, H, W); 
+  ecriture_pgm(nom_out, xrec, W, H);
+  
+  dalocd(xrecd,H);
+  dalocd(xd,H);
+  dalocd(tdct,H);
+  
+
+  //FIN CODE DCT
+  */
+
+// DCT PAR BLOCS
+/*
+double **tdct = alocamd(H, W); // image transformee
+double **xd = alocamd(H, W);   // image initiale en double
+double **xrecd = alocamd(H, W); // image reconstruite
+
+  for(i=0;i<H;i++)
+    for(j=0;j<W;j++)
+      xd[i][j] = (double)x[i][j]; 
+
+
+
+int Bx=64, By=64; 
+
+dct2dim_bloc(xd, tdct, H, W, Bx, By, step); 
+
+for(i=0;i<H;i++)
+  for(j=0;j<W;j++)
+    err[i][j] = (int)tdct[i][j];
+
+double entro = calc_entropie(err, H, W); 
+fprintf(stderr, "\nentropie = %g [bits/pixel]\n", entro);
+
+dct2dim_bloc_inv(tdct, xrecd, H, W, Bx, By); 
+
+for(i=0;i<H;i++)
+    for(j=0;j<W;j++)
+      if(xrecd[i][j] < 0.0)  xrec[i][j] = 0;
+      else if(xrecd[i][j] > 255.0)  xrec[i][j] = 255;
+      else  xrec[i][j] = (unsigned char)xrecd[i][j]; 
+
+  SaveIntImage_pgm_tronc(nom_err, err, H, W); 
+  ecriture_pgm(nom_out, xrec, W, H);
+   dalocd(xrecd,H);
+  dalocd(xd,H);
+  dalocd(tdct,H);
+*/
+
+
+// FIN DCT PAR BLOCS
+
+
+//
+  
+
+//PREDICTION
+  
+   my_codeurDPCM(x, err, H, W, step); 
+   my_decodeurDPCM(err, xrec, H, W); 
+
+  
+   //  codeur_adapt(x, err, H, W, step);  
+   //    decodeur_adapt(err, xrec, H, W);  
+   
+  
+   double entro = calc_entropie(err, H, W); 
+   fprintf(stderr, "\nentropie = %g [bits/pixel]\n", entro);
+
+
+    my_codeurDPCM_retro(x, err, H, W, step);
+    my_decodeurDPCM(err, xrec, H, W); 
+    
+  
+    SaveIntImage_pgm(nom_err, err, H, W); 
+  
+
+  ecriture_pgm(nom_out, xrec, W, H);
+
+//FIN PREDICTION
+   
+   dalocuc(x,H);
+   dalocuc(xrec,H);
+  daloci(err,H);
+
+  return 1; 
+}
+
+/*
+void my_codeurDPCM(unsigned char **x, int **err, int H, int W, int step)
+{
+//Fill this function ! 
+}
+
+void my_decodeurDPCM(int **err, unsigned char **xrec, int H, int W)
+{
+//Fill this function ! 
+}
+
+
+*/
+void my_codeurDPCM(unsigned char **x, int **err, int H, int W, int step)
+{
+    int i, j;
+    int pred;
+
+    for(i = 0; i < H; i++)
+    {
+        for(j = 0; j < W; j++)
+        {
+            if(j == 0)
+                pred = 0; // pas de voisin gauche
+            else
+                pred = x[i][j-1];
+
+            // erreur de prédiction
+            int e = (int)x[i][j] - pred;
+
+            // quantification
+            err[i][j] = (int)quantiz((double)e, step);
+        }
+    }
+}
+void my_decodeurDPCM(int **err, unsigned char **xrec, int H, int W)
+{
+    int i, j;
+    int pred;
+
+    for(i = 0; i < H; i++)
+    {
+        for(j = 0; j < W; j++)
+        {
+            if(j == 0)
+                pred = 0;
+            else
+                pred = xrec[i][j-1];
+
+            // reconstruction
+            int val = pred + err[i][j] ;
+
+            // clamp entre 0 et 255
+            if(val < 0) val = 0;
+            if(val > 255) val = 255;
+
+            xrec[i][j] = (unsigned char)val;
+        }
+    }
+}
+
+int calc_err(unsigned char val_1, unsigned char val_2, int step) {
+ double err = val_1 - val_2; 
+ return quantiz(err,step);
+}
+
+void my_codeurDPCM_retro(unsigned char **x, int **err, int H, int W, int step)
+{
+ int i, j; 
+
+ for (i = 0 ; i < H ; i++) { 
+    for (j = 0; j < W ; j++) { 
+ if (j == 0){
+ err[i][j] = quantiz(x[i][j], step);
+ x[i][j] = err[i][j];
+ } else {
+ err[i][j] = calc_err(x[i][j], x[i][j - 1], step);
+ x[i][j] = err[i][j] + x[i][j - 1]; 
+ }
+ }
+ }
+}
   dalocuc(ima_uc, Height); 
   
   return 1; 
